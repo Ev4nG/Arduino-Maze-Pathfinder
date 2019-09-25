a26d3d5e77ac057cd6466fb3379e07c48c8d6b99
Evan G <55809800+Ev4nG@users.noreply.github.com>
2019-09-26 00:07:32 +0100

    Add files via upload

---
+++ Pathfinder.ino
@@ -0,0 +1,50 @@
+#include <Servo.h>
+
+Servo servoLeft;
+Servo servoRight;
+
+
+
+void setup()
+{
+  tone(4, 2000, 1000);
+  delay(1000);
+  
+  pinMode(7, INPUT);
+  pinMode(5, INPUT);
+    Serial.begin(9600);
+  
+   servoLeft.attach(13);
+   servoRight.attach(12);
+  
+  turnLeft =  servoLeft.writeMicroseconds(1300);
+               servoRight.writeMicroseconds(1300);
+               delay(600);
+
+
+  
+  servoLeft.detach();
+  servoRight.detach();
+  
+}
+
+
+void loop() 
+{
+  byte wLeft = digitalRead(5);
+  byte wRight = digitalRead(7);
+  
+   if((wLeft == 0) && (wRight == 0))
+  {
+    backward(1000);
+    turnLeft(400);
+    
+  }
+  
+  Serial.print(wLeft);
+  Serial.println(wRight);
+  
+ 
+  
+  delay(50);
+}
